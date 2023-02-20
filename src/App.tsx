668537d7b249d0ec16a8ed6f343c752d827d8ca2
Derek Naing <dereknaing01@gmail.com>
2023-02-20 00:32:30 -0800

    Initializing repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
+
+
+
+import "./fonts/Starjedi.ttf";
+import "./fonts/Starjhol.ttf";
+import "./fonts/FranklinGothic.otf";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Navbar/> */}
+      {/* <span className="heading"> star wars compendium </span>
+      <span className="body"> Hello, welcome to the Star Wars Compendium. </span> */} 
     </div>
   );
 }
