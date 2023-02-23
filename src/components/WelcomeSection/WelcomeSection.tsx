9cfca6c38187cf086c968505e96eca78398ab772
Derek Naing <dereknaing01@gmail.com>
2023-02-23 03:03:57 -0800

    Added background video in home screen

--- src/components/WelcomeSection/WelcomeSection.tsx
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-// import videoBg from '../../assets/videos/hyperspace.mp4';
+import videoBg from '../../assets/videos/hyperspace.mp4';
 
 import '../../App.css';
 import './WelcomeSection.css';
@@ -9,7 +9,7 @@ import './WelcomeSection.css';
 function WelcomeSection() {
     return (
       <div className='welcome-container'>
-        
+        <video src={videoBg} autoPlay loop muted />
       </div>
     );
   }
