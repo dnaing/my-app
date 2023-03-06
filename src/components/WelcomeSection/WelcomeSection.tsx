43e5202ebc1b15811153fa61eecc5978d6134436
Derek Naing <dereknaing01@gmail.com>
2023-03-05 21:04:12 -0800

    Added welcome hero image

--- src/components/WelcomeSection/WelcomeSection.tsx
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import videoBg from '../../assets/videos/hyperspace.mp4';
+import image from '../../assets/images/welcomeImage.jpg';
 
 import '../../App.css';
 import './WelcomeSection.css';
@@ -9,7 +10,17 @@ import './WelcomeSection.css';
 function WelcomeSection() {
     return (
       <div className='welcome-container'>
-        <video src={videoBg} autoPlay loop muted />
+
+          <div className='welcome-hero-container'>
+            <img className='welcome-hero' src={image} loading='lazy'/>
+            <div className='welcome-hero-text'>
+              <h1> Welcome! </h1>
+            </div>
+          </div>
+
+          {/* <img className='films-hero' src={image}/>
+          <h2>Films</h2> */}
+
       </div>
     );
   }
